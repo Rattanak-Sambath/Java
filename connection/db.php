22776c6c9c9d385573a61f67937ca4c8deda54a1
Sophea Vann <msophea99@gmail.com>
2021-03-27 11:01:06 +0700

    update connection

--- connection/db.php
+++ connection/db.php
@@ -1,8 +1,15 @@
 <?php
 $server = "localhost";
+// local
 $username = "root";
 $password = "";
 $database = "pagoda";
+// 
+// webhost
+// $username = "id16458928_sophea";
+// $password = "5n+Dz?v[fkg03~9p";
+// $database = "id16458928_pagoda";
+// 
 $conn = new mysqli($server, $username, $password, $database);
 if ($conn->connect_error) {
   die("Connected failed" . $conn->connect_error);
---
+++ connection/db_webhost.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$username = "id16458928_sophea";
+$password = "5n+Dz?v[fkg03~9p";
+$database = "id16458928_pagoda";
+$conn = new mysqli($server, $username, $password, $database);
+if ($conn->connect_error) {
+  die("Connected failed" . $conn->connect_error);
+}
