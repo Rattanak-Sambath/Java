2d4c94d0878fac263579d054452d221e577cdf15
Sophea Vann <msophea99@gmail.com>
2021-04-02 10:45:08 +0700

    page for client

---
+++ public/action/index_action.php
@@ -0,0 +1,50 @@
+<?php
+$received_data = json_decode(file_get_contents("php://input"));
+$data = array();
+// connection
+include '../../connection/db.php';
+// 
+// get invoice by month and year
+if ($received_data->action == 'getInvoice') {
+  $month = $received_data->month;
+  $year = $received_data->year;
+  // sql
+  $sql = "SELECT tbl_invoice.id, tbl_invoice.ele_old, tbl_invoice.ele_new, tbl_invoice.water_old, tbl_invoice.water_new,
+  tbl_person.name, tbl_person.phone, tbl_person.latin,
+  tbl_home.name as homeName
+  FROM tbl_invoice
+  INNER JOIN tbl_person
+  ON tbl_person.id=tbl_invoice.person_id
+  INNER JOIN tbl_home
+  ON tbl_person.home_id=tbl_home.id
+  WHERE tbl_invoice.month='$month' and tbl_invoice.year=$year";
+  // 
+  $result = mysqli_query($conn, $sql);
+  // 
+  while ($row = $result->fetch_array()) {
+    $data[] = $row;
+  }
+
+
+  // return
+  echo json_encode($data);
+}
+// get unit price
+if ($received_data->action == 'getUnitPrice') {
+  $month = $received_data->month;
+  $year = $received_data->year;
+  // sql
+  $sql = "SELECT * FROM tbl_unit_price
+  WHERE month='$month' AND year=$year limit 1";
+  // 
+  $result = mysqli_query($conn, $sql);
+  if ($result && mysqli_num_rows($result) > 0) {
+    $unit_data = mysqli_fetch_assoc($result);
+    echo json_encode($unit_data);
+  } else {
+    echo json_encode("no data");
+  }
+
+
+  // echo json_encode($unit_data);
+}
--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Vue</title>
+  <title>Electric-Water</title>
   <!-- quasar -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet" type="text/css">
   <link href="https://cdn.jsdelivr.net/npm/quasar@1.15.7/dist/quasar.min.css" rel="stylesheet" type="text/css">
@@ -21,7 +21,6 @@
             <div class="text-h5">
               Kandal-Pagoda
             </div>
-
           </q-toolbar-title>
           <!-- right side -->
           <q-btn dense label="Login" color="positive" @click="goLogin()" />
@@ -46,28 +45,23 @@
 
                 <!-- select year -->
                 <div v-show="!checkboxText" style="width: 200px;" class="q-pa-sm">
-                  <q-select outlined v-model="year" dense options-dense :options="years" label="Select Year" clearable />
+                  <q-select @input="searchInvoice()" outlined v-model="year" dense options-dense :options="years" label="Select Year" clearable />
                 </div>
 
                 <!-- input -->
                 <div v-show="checkboxText" style="width: 200px;" class="q-pa-sm">
-                  <q-input outlined type="number" v-model.number="year" dense label="Input Year" clearable />
+                  <q-input @input="searchInvoice()" outlined type="number" v-model.number="year" dense label="Input Year" clearable />
                 </div>
 
                 <!-- select month -->
                 <div style="width: 200px;" class="q-pa-sm">
-                  <q-select outlined v-model="month" dense options-dense :options="months" label="Select Month" clearable />
+                  <q-select @input="searchInvoice()" outlined v-model="month" dense options-dense :options="months" label="Select Month" clearable />
                 </div>
 
-                <!-- btn search -->
+                <!-- btn refresh -->
                 <div class="q-pa-sm">
-                  <q-btn label="Search" color="primary" @click="getAllMonths()" />
+                  <q-btn label="Refresh" color="primary" @click="onRefresh()" />
                 </div>
-
-
-
-
-
 
               </q-card-section>
             </div>
@@ -77,13 +71,17 @@
             <!--  -->
             <div>
               <q-card-section>
-                <div class="row q-col-gutter-x-md q-col-gutter-y-sm">
-                  <div class="col-xs-6 col-sm-6 col-md-3" v-for="(person,index) in persons" :key="index">
+                <div class="row q-col-gutter-x-md q-col-gutter-y-md">
+                  <div class="col-xs-12 col-sm-6 col-md-3" v-for="(person,index) in persons" :key="index">
                     <q-card bordered flat class="my-card">
                       <div>
-                        <!-- month -->
-                        <q-card-section class="text-h5 row bg-positive">
-                          {{ person.name }}
+                        <!-- name -->
+                        <q-card-section :class="unit.ele>1?'bg-positive':'bg-negative'">
+                          <q-btn flat :label="person.latin" class="text-bold" no-caps>
+                            <q-tooltip content-class="bg-primary text-bold" content-style="font-size: 16px" :offset="[10, 10]" anchor="center right" self="center start">
+                              {{ person.name }} - {{ person.homeName }}
+                            </q-tooltip>
+                          </q-btn>
                         </q-card-section>
                       </div>
                       <div>
@@ -91,19 +89,114 @@
                       </div>
                       <div>
                         <q-card-section>
+                          <!-- electric -->
+                          <div class="text-bold">
+                            ចំនួនប្រើប្រាស់ភ្លើង :
+                            <q-badge color="red" class="text-white text-bold">
+                              {{ person.ele_old }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                Old
+                              </q-tooltip>
+                            </q-badge>
+                            -
+                            <q-badge color="blue" class="text-white text-bold">
+                              {{ person.ele_new }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                New
+                              </q-tooltip>
+                            </q-badge>
+                            :
+                            <q-badge color="green" class="text-white text-bold">
+                              {{ person.ele_new - person.ele_old }}
+                              <q-tooltip content-class="bg-primary text-bold" anchor="top middle" self="center middle">
+                                Total
+                              </q-tooltip>
+                            </q-badge>
+                          </div>
+
+                          <q-separator></q-separator>
+
+                          <!-- electric finish -->
+                          <div v-show="unit.ele>1?true:false" class="text-bold">
+                            តម្លៃភ្លើង (
+                            <q-badge color="amber" class="text-black text-bold">
+                              {{ unit.ele }}
+                            </q-badge>
+                            R ) :
+                            <q-badge color="green" class="text-white text-bold">
+                              {{ (person.ele_new - person.ele_old) * unit.ele }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                Money
+                              </q-tooltip>
+                            </q-badge> R
+                          </div>
+
+
+                          <q-separator></q-separator>
+
+
+
+                          <!-- water -->
+                          <div class="text-bold">
+                            ចំនួនប្រើប្រាស់ទឹក :
+                            <q-badge color="red" class="text-white text-bold">
+                              {{ person.water_old }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                Old
+                              </q-tooltip>
+                            </q-badge>
+                            -
+                            <q-badge color="blue" class="text-white text-bold">
+                              {{ person.water_new }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                New
+                              </q-tooltip>
+                            </q-badge>
+                            :
+                            <q-badge color="green" class="text-white text-bold">
+                              {{ person.water_new - person.water_old }}
+                              <q-tooltip content-class="bg-primary text-bold" anchor="top middle" self="center middle">
+                                Total
+                              </q-tooltip>
+                            </q-badge>
+                          </div>
+
+
+
+                          <q-separator></q-separator>
+
+                          <!-- water finish -->
+                          <div v-show="unit.water>1?true:false" class="text-bold">
+                            តម្លៃទឹក (
+                            <q-badge color="amber" class="text-black text-bold">
+                              {{ unit.water }}
+                            </q-badge>
+                            R ) :
+                            <q-badge color="green" class="text-white text-bold">
+                              {{ (person.water_new - person.water_old) * unit.water }}
+                              <q-tooltip content-class="bg-amber text-black text-bold" anchor="top middle" self="center middle">
+                                Money
+                              </q-tooltip>
+                            </q-badge> R
+                          </div>
+
+                          <q-separator></q-separator>
+
+                          <div class="text-bold">
+                            Phone :
+                            <q-badge>
+                              {{ person.phone }}
+                            </q-badge>
+                          </div>
 
                         </q-card-section>
+
+
                       </div>
+
 
                     </q-card>
                   </div>
-
-
-
-
-
-
-
 
 
                 </div>
@@ -120,7 +213,99 @@
   <script src="https://cdn.jsdelivr.net/npm/vue@^2.0.0/dist/vue.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/quasar@1.15.7/dist/quasar.umd.min.js"></script>
 
-  <script src="./js/index.js"></script>
+  <script>
+    var app = new Vue({
+      el: "#q-app",
+      name: "ex",
+      data: function() {
+        return {
+          year: null,
+          years: [],
+          checkboxText: false,
+          month: "",
+          months: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          persons: [],
+          unit: {
+            ele: 1,
+            water: 1,
+          },
+        };
+      },
+      created() {
+        this.generateYear();
+      },
+      methods: {
+        onRefresh() {
+          this.searchInvoice()
+        },
+        goLogin() {
+          window.location.href = "../login.php";
+        },
+        generateYear() {
+          //
+          this.month = this.months[new Date().getMonth()];
+          //
+          this.year = new Date().getFullYear();
+          this.years = [];
+          thisYear = new Date().getFullYear();
+          for (i = 0; i < 5; i++) {
+            this.years.push(thisYear);
+            thisYear += 1;
+          }
+          // 
+          this.getAllInvoices(this.month, this.year)
+          this.getUnitPrice(this.month, this.year)
+        },
+        searchInvoice() {
+          this.getAllInvoices(this.month, this.year);
+          this.getUnitPrice(this.month, this.year);
+
+        },
+        getAllInvoices(month, year) {
+          // 
+          axios.post("action/index_action.php", {
+            action: "getInvoice",
+            month: month,
+            year: year,
+          }).then(res => {
+            this.persons = res.data
+          })
+        },
+        getUnitPrice(month, year) {
+          // 
+          axios.post("action/index_action.php", {
+            action: 'getUnitPrice',
+            month: month,
+            year: year,
+          }).then(res => {
+            if (res.data == 'no data') {
+              this.unit.ele = 1
+              this.unit.water = 1
+
+            } else {
+              this.unit.ele = res.data.ele;
+              this.unit.water = res.data.water;
+            }
+          })
+
+
+        }
+      },
+    });
+  </script>
 </body>
 
 </html>
